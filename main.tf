b9cef794b91ab2b6851228c58d856d5a396a36cc
ayahmuhamed <88331884+ayahmuhamed@users.noreply.github.com>
2026-02-04 12:48:23 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,6 @@
+resource "null_resource" "null" {}
+resource "null_resource" "null1"{}
+resource "null_resource" "null3"{}
+resource "null_resource" "null4"{}
+resource "null_resource" "null5"{}
+resource "null_resource" "null6"{}
